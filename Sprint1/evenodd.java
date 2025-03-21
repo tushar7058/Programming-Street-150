5ff498db8dde0393665e3a2233cb3e5a0ff1d25b
tushar7058 <tusharkale816@gmail.com>
2025-03-21 12:56:35 +0530

    changes

---
+++ Sprint1/evenodd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class evenodd{
+	public static void main(String[]args){
+		int num = 10;
+		Evenodd(num);
+	}
+	static void Evenodd(int num){
+		if(num%2==0){
+			System.out.println("The num is Even");
+		}else{
+			System.out.println("The num is Odd");
+		}
+	}
+
+}
